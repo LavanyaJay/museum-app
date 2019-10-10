1d3709927d6e7e44b18a5ca99df02c6805763071
LavanyaJay <lavanyajay75@gmail.com>
2019-10-10 11:18:44 +0200

    Capture comments from user

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,22 @@
 function submitComment(){
-    console.log("Hello")
+    
+    const inputField = document.getElementById("name");
+    console.log("Input Field");
+    const name = inputField.value;
+    console.log(name);
+    const msgtext = document.getElementById("msg");
+    console.log(msgtext);
+    const msg = msgtext.value;
+    console.log(msg)
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = msg;
+    comment.classList.add("comment");
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment);
+
+
 }
